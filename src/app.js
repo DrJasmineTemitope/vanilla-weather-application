12ced7d9a93fca2579a9c226e9e49092380edcf9
DrJasmineTemitope <120563093+DrJasmineTemitope@users.noreply.github.com>
2022-12-23 20:08:25 +0100

    Updating the app JavaScript elements

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+function displayTemperature(response) {
+  console.log(response);
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
 
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].main;
+}
+
+let apiKey = "f029b2b922567275529f515833feaaac";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
